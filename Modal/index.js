60f306a353a4c4c6478e75c82a1fac7a84944494
Faith Makinde <faithmakinde0@gmail.com>
2022-09-06 15:58:01 +0100

    Modal project completed the javascript

---
+++ Modal/index.js
@@ -0,0 +1,7 @@
+
+document.getElementById("close-modal").addEventListener("click", function(){
+    document.getElementById("overlay").style.display = "none";
+})
+document.getElementById("open-modal").addEventListener("click", function(){
+    document.getElementById("overlay").style.display = "block";
+})
