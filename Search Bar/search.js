a8ab3c158bdb64d4bd931a117d1d1b22e4e70a94
Faith Makinde <faithmakinde0@gmail.com>
2022-09-10 00:14:21 +0100

    completed

--- Search Bar/search.js
+++ Search Bar/search.js
@@ -1,11 +1,13 @@
- let searchEl = document.getElementById("searchInput");
- let allNamesDOMCollection = "";
-searchEl.addEventListener("keyup", function(event){
-    let username = event.target.value.toLowerCase();
-    allNamesDOMCollection = document.getElementsByClassName("name");
+
+   document.getElementById("searchInput").addEventListener("keyup", function(event){
+    let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName("name");
     for(let counter = 0; counter < allNamesDOMCollection.length; counter++){
         const currentName = allNamesDOMCollection[counter].textContent;
-        console.log(currentName.toLowerCase())
+        if (currentName.includes(searchQuery)) {
+            allNamesDOMCollection[counter].style.display = "block";
+        } else {
+            allNamesDOMCollection[counter].style.display = "none";
+        }
     }
-    // console.log(allNamesDOMCollection[5].textContent)
  })
