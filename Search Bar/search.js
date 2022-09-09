daf8feafad53bc7849452644a7ba33e03af55482
Faith Makinde <faithmakinde0@gmail.com>
2022-09-09 23:50:45 +0100

    loop through all name

--- Search Bar/search.js
+++ Search Bar/search.js
@@ -1,5 +1,11 @@
- let inputEl = document.getElementById("usernameInput")
-inputEl.addEventListener("keyup", function(event){
+ let searchEl = document.getElementById("searchInput");
+ let allNamesDOMCollection = "";
+searchEl.addEventListener("keyup", function(event){
     let username = event.target.value.toLowerCase();
-    console.log(username)
+    allNamesDOMCollection = document.getElementsByClassName("name");
+    for(let counter = 0; counter < allNamesDOMCollection.length; counter++){
+        const currentName = allNamesDOMCollection[counter].textContent;
+        console.log(currentName.toLowerCase())
+    }
+    // console.log(allNamesDOMCollection[5].textContent)
  })
