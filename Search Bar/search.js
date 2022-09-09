6b75b97aaf4bdb92b84873a5f0bb49e1d3faac3e
Faith Makinde <faithmakinde0@gmail.com>
2022-09-10 00:18:01 +0100

    bug fixed

--- Search Bar/search.js
+++ Search Bar/search.js
@@ -3,7 +3,7 @@
     let searchQuery = event.target.value.toLowerCase();
     let allNamesDOMCollection = document.getElementsByClassName("name");
     for(let counter = 0; counter < allNamesDOMCollection.length; counter++){
-        const currentName = allNamesDOMCollection[counter].textContent;
+        const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
         if (currentName.includes(searchQuery)) {
             allNamesDOMCollection[counter].style.display = "block";
         } else {
