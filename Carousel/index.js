af48dc5d0b45e52dc46acdda3518bf9481a845c4
Faith Makinde <faithmakinde0@gmail.com>
2022-09-11 23:59:14 +0100

    implemented javascript

---
+++ Carousel/index.js
@@ -0,0 +1,13 @@
+const slides = document.getElementsByClassName("carousel-item");
+let slidePosition =  0;
+const totalSlides = slides.length;
+
+document.getElementById("carousel-button-prev").addEventListener("click", moveToPrevSlide);
+document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide)
+function moveToPrevSlide(){
+   
+}
+function moveToNextSlide(){
+
+}
+console.log(totalSlides);
